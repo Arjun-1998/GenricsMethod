8f496c3470c44296c77796cba8524bb8a26c2756
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-12-07 17:55:53 +0530

    [Raju] Add ; Added Evaluated the maximum number

---
+++ GenericsMethod/GenericsMaximum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genric
+{
+    public class GenricMaximum<T> where T : IComparable
+    {
+
+        public T MaxMethod(T[] value)
+        {
+            var max = MaxValue(value);
+            return max;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(T[] values)
+        {
+            var sorted_value = Sort(values);
+            return sorted_value[sorted_value.Length - 1];
+        }
+
+        public T PrintMaxValue(T[] value)
+        {
+            var max = MaxValue(value);
+            Console.WriteLine("Maximum value is" + max);
+            return max;
+        }
+    }
+
+}
---
+++ GenericsMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using Genric;
+
+class program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Find Maximum Number!");
+
+        int[] intArray = { 1, 2, 9, 4 };
+        GenricMaximum<int> generic = new GenricMaximum<int>();
+        generic.PrintMaxValue(intArray);
+
+
+
+
+
+
+    }
+}
