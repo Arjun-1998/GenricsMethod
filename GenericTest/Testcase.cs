c10bb83e8a2f4cdd5d3c5268c7088cd4cd72b445
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-12-08 00:14:05 +0530

    [Arjun] Add ; Added Executed

--- GenericTest/Testcase.cs
+++ GenericTest/Testcase.cs
@@ -5,18 +5,32 @@ namespace GenricTest
 {
     public class Testcase
     {
-       
+
 
         [Test]
+        public void GivenMaxIntFirst_WhenPrintMaxValue_ThenShouldGiveFirstValue()
+        {
+            int[] intArray = { 9, 1, 2, 4 };
+            int expected = intArray[0];
+            GenricMaximum<int> genric = new GenricMaximum<int>();
+            int result = genric.PrintMaxValue(intArray);
+            Assert.AreEqual(expected, result);
+        }
         public void GivenMaxIntSecond_WhenPrintMaxValue_ThenShouldGiveSecondValue()
         {
-            int[] intArray = { 1, 9, 2, 4 };
+            int[] intArray = { 2, 9, 1, 4 };
             int expected = intArray[1];
             GenricMaximum<int> genric = new GenricMaximum<int>();
             int result = genric.PrintMaxValue(intArray);
-            Assert.AreEqual(expected,result);
+            Assert.AreEqual(expected, result);
         }
-
-
+        public void GivenMaxIntThird_WhenPrintMaxValue_ThenShouldGiveThirdValue()
+        {
+            int[] intArray = { 2, 1, 9, 4 };
+            int expected = intArray[2];
+            GenricMaximum<int> genric = new GenricMaximum<int>();
+            int result = genric.PrintMaxValue(intArray);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
