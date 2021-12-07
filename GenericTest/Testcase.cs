333aaa1194daade75e26366234d6953ecca04db0
Arjun-1998 <arjunjaiswal8065@gmail.com>
2021-12-08 00:03:31 +0530

    [Arjun] Add ; Added Maximum Number At Second Position

--- GenericTest/Testcase.cs
+++ GenericTest/Testcase.cs
@@ -8,10 +8,10 @@ namespace GenricTest
        
 
         [Test]
-        public void GivenMaxIntFirst_WhenPrintMaxValue_ThenShouldGiveFirstValue()
+        public void GivenMaxIntSecond_WhenPrintMaxValue_ThenShouldGiveSecondValue()
         {
-            int[] intArray = { 9, 1, 4, 2 };
-            int expected = intArray[0];
+            int[] intArray = { 1, 9, 2, 4 };
+            int expected = intArray[1];
             GenricMaximum<int> genric = new GenricMaximum<int>();
             int result = genric.PrintMaxValue(intArray);
             Assert.AreEqual(expected,result);
